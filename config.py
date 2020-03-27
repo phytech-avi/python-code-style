953d1aede2719673492e061f3486bb8bac4c6748
und3v3l0p3d <prohibitme@phygitalism.com>
2020-03-27 11:58:31 +0300

    change to config.py

--- config.py
+++ config.py
@@ -10,3 +10,16 @@ class Config:
     """
     DEBUG = False
     DB_STORAGE_PATH = os.path.join("storage", "db_storage.sqlite")
+
+
+class BaseConfig(Config):
+    """ Base Run Config
+    """
+    BUG = False
+
+
+class TestConfig:
+    """Description for what context this config
+    """
+    DEBUG = True
+    DB_STORAGE_PATH = "test/db_storage.sqlite"
